2ef7f1d6eea1ffd7f9a1d716e96fb22422776cb9
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2026-06-01 12:39:28 +0700

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// 👇 TARUH DI SINI (PALING ATAS)
-router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-
-  next();
-});
 const {
   getProducts,
   addProduct,
