19a9518d1177a60d874a90ff2fb82347fcd3022a
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2026-06-01 10:54:07 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,4 +20,12 @@ app.get("/", (req, res) => {
   });
 });
 
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
 module.exports = app;
