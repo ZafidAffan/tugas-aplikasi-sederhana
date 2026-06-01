2e762f23f5c3f24f2440ffe670c44022570f9bce
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2026-06-01 12:19:20 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,18 +8,6 @@ const productRoutes = require("../routes/productRoutes");
 
 const app = express();
 
-// ✅ CORS FIX
-app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "http://127.0.0.1:5500",
-        "https://pab-forntend-r637.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-// ❌ JANGAN pakai app.options("*")
 
 app.use(express.json());
 
