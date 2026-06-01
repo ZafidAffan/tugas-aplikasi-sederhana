a8b6b4469455560a570f03ba248c92ccd02d16c3
ZafidAffan <155897126+ZafidAffan@users.noreply.github.com>
2026-06-01 10:51:56 +0700

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,16 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKeyPAB.json");
+
+let serviceAccount;
+
+if (process.env.VERCEL) {
+  serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  };
+} else {
+  serviceAccount = require("../serviceAccountKeyPAB.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
